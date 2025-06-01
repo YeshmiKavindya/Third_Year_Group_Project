c7d777db421d6aa359a76dbfbc259760aa56e4ff
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2025-06-01 07:50:20 +0000

    update login.tsx

---
+++ TaproHQ/app/login.tsx
@@ -0,0 +1,18 @@
+import { View ,StyleSheet} from "react-native";
+export default function Login() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  )
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "orange",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    },})
