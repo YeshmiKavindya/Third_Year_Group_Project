9c9c5f350d9cc2cd8a63c38e9943942b004c791e
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2025-06-16 12:49:35 +0000

    added gitignore

--- TaproHQ/App.tsx
+++ TaproHQ/App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text,StatusBar } from 'react-native';
+import Login from './screens/Login';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar 
+        backgroundColor="orange"
+        barStyle={"dark-content"}
+        hidden={false}
+      />
+
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{ headerShown: false }} 
+        />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*
+
+
+    
+    <NavigationContainer>
+      <StatusBar 
+        backgroundColor="orange"
+        barStyle={"dark-content"}
+        hidden={false}
+      />
+
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Sign" 
+          component={Sign}
+          options={{ headerShown: true, title: 'Login' }} 
+        />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+*/
---
+++ TaproHQ/screens/Login.tsx
@@ -0,0 +1,128 @@
+import { ImageBackground } from "expo-image";
+import { View ,StyleSheet,Text,TextInput,TouchableOpacity} from "react-native";
+import { Link } from "expo-router";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { openURL } from "expo-linking";
+
+export default function Login() {
+  return (
+    <View>
+        <Text>Login page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    },
+
+    card: {
+    padding: 20,
+    borderRadius: 14,
+    justifyContent: "center",
+  },
+   title: {
+    textAlign: "center",
+    fontSize: 30,
+    color:'white',
+    padding: 10,
+    fontWeight: "bold",
+  },
+  input: {
+    marginBottom: 15,
+    height: 55,
+    width: 280,
+    borderRadius: 14,
+    borderWidth: 2,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    justifyContent: "center",
+    backgroundColor: "gray",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    height: 55,
+  },
+  text: {
+    textAlign: "center",
+    padding: 10,
+    color:'white',
+    fontWeight:'bold',
+    
+  },
+  background: {
+    height:'auto',
+    width:'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  link:{
+    color: "#D2B48C",
+    fontWeight:'bold',
+
+  },
+  line: {
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+  
+})
+
+
+/*
+
+    <View style={styles.container}>
+    <ImageBackground source={require('../assets/images/loginimg.png')}
+    style={styles.background}>
+        <View style={styles.card}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput placeholder="Username" style={styles.input} />
+            <TextInput placeholder="Password" style={styles.input} />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+          <Text style={styles.text}>
+            Forgot Password?
+          </Text>
+          </TouchableOpacity>
+          <Text style={styles.text}>
+          Don't have an account?{" "}
+          <Link href={"/signup"} style={styles.link}>
+            register now
+          </Link>
+        </Text>
+        <View style={styles.line} />
+        <View style={{flexDirection: "row", gap:30,justifyContent:'center'}}>
+            <TouchableOpacity onPress={()=>openURL('https://www.facebook.com')}>
+                <Icon name="facebook" size={30} color={'white'}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>openURL('https://www.instagram.com')}>
+                <Icon name="instagram" size={30} color={'white'} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>openURL('https://www.google.com')}>
+                <Icon name="google" size={30} color={'white'}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Icon name="phone" size={30} color={'white'} />
+            </TouchableOpacity>
+        </View>
+        
+      
+
+        </View>
+        </ImageBackground>
+    
+    </View>
+*/
