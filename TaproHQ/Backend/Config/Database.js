1445138f315d0fcf48fa022ca1fc5718adc09024
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2025-08-02 13:49:08 +0000

    Creates a try block to catch any errors that might happen during connection.

--- TaproHQ/Backend/Config/Database.js
+++ TaproHQ/Backend/Config/Database.js
@@ -4,4 +4,5 @@ const connectDB = async () => {
     if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI environment variable is not defined');
   }
-  
+    try {
+    console.log('Attempting to connect to MongoDB...');
