60d47afd08229dfe0182f90e8241ed67f2dbbd7f
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2025-08-02 13:50:00 +0000

    Created a new instance Mongoclient.

--- TaproHQ/Backend/Config/Database.js
+++ TaproHQ/Backend/Config/Database.js
@@ -6,3 +6,10 @@ const connectDB = async () => {
   }
     try {
     console.log('Attempting to connect to MongoDB...');
+    const client = new MongoClient(process.env.MONGO_URI, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
