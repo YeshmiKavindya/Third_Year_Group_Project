8397b9e755f9eaf02be7479f450428c9a95f0b07
MumtazAysha <139667400+MumtazAysha@users.noreply.github.com>
2025-08-02 13:47:54 +0000

    Check if the environment variable MONGO_URI exists.

--- TaproHQ/Backend/Config/Database.js
+++ TaproHQ/Backend/Config/Database.js
@@ -1,3 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const connectDB = async () => {
+    if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI environment variable is not defined');
+  }
+  
