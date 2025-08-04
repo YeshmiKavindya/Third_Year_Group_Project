149e5a84e6d91c65783271de34e378b2292c3ed1
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2025-08-04 16:40:28 +0000

    add location & joined_date

--- TaproHQ/Backend/models/userInfo.js
+++ TaproHQ/Backend/models/userInfo.js
@@ -4,7 +4,9 @@ const userInfoSchema = new mongoose.Schema({
   user_name: { type: String, required: true },
   email: { type: String, required: true , unique: true },
   user_type: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  joined_date: { type: Date, default: Date.now },
+  location: { type: String, required: false }
 });
 
 module.exports = mongoose.model('userInfo', userInfoSchema);
