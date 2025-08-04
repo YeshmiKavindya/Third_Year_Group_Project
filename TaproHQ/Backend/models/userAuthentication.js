910bce52f4fac76e7c7832ccaadf3696360d4c4b
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2025-08-04 16:42:36 +0000

    add tooken field

--- TaproHQ/Backend/models/userAuthentication.js
+++ TaproHQ/Backend/models/userAuthentication.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userAuthenticationSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     user_type: {type: String, required: true },
-    
+    token: { type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model('userAuthentication', userAuthenticationSchema);
