802ca28422d5708418669329d898389edf0d3779
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2025-08-04 16:43:09 +0000

    add logindate

--- TaproHQ/Backend/models/userAuthentication.js
+++ TaproHQ/Backend/models/userAuthentication.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userAuthenticationSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     user_type: {type: String, required: true },
-    token: { type: String, required: true, unique: true }
+    token: { type: String, required: true, unique: true },
+    login_date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('userAuthentication', userAuthenticationSchema);
